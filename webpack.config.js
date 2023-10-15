430374aae2eb7b5fd4dcc73f22fbaa2925ee5e04
Honi <1095175021@qq.com>
2023-10-15 22:00:16 +0800

    feat: 回退版本，基础打包配置

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    // path.resolve 将一个路径转换为绝对路径, __dirname 当前执行文件的绝对路径
+    path: path.resolve(__dirname, 'dist'),
+  },
+}
