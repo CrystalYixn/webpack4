37fe595ae9b4688d3bf685e9792034d28e43b607
Honi <1095175021@qq.com>
2023-10-16 00:25:03 +0800

    feat: 配置开发服务器

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,9 @@ module.exports = {
     // path.resolve 将一个路径转换为绝对路径, __dirname 当前执行文件的绝对路径
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    port: 3000,
+    progress: true,
+    contentBase: './build',
+  },
 }
